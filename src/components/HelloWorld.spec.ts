e0dd03561d0edb77ce64a485829f7cda0b503368
Portelah <lucasbpcaetano@outlook.com>
2023-02-23 16:47:53 -0400

    Adicionado botão de alterar o texto

--- src/components/HelloWorld.spec.ts
+++ src/components/HelloWorld.spec.ts
@@ -11,3 +11,52 @@ test("should validate App the componnent", async () => {
   });
   expect(wrapper.text()).toContain("Hello From Test");
 });
+
+test("should validate App the componnent", async () => {
+  expect(HelloWord).toBeTruthy();
+  const wrapper = mount(HelloWord, {
+    props: {
+      msg: "Hello From Test",
+    },
+  });
+  expect(wrapper.text()).toContain("count is 0");
+});
+
+test("should validate App the componnent", async () => {
+  expect(HelloWord).toBeTruthy();
+  const wrapper = mount(HelloWord, {
+    props: {
+      msg: "Hello From Test",
+    },
+  });
+  await wrapper.get("button").trigger("click");
+  expect(wrapper.text()).toContain("count is 1");
+});
+
+test("should validate App the componnent", async () => {
+  expect(HelloWord).toBeTruthy();
+  const wrapper = mount(HelloWord, {
+    props: {
+      msg: "Hello From Test",
+    },
+  });
+  for (let index = 0; index < 10; index++) {
+    await wrapper.get("button").trigger("click");
+  }
+  expect(wrapper.text()).toContain("count is 10");
+});
+
+test("should overwrite the text on page", async () => {
+  expect(HelloWord).toBeTruthy();
+
+  const wrapper = mount(HelloWord, {
+    props: {
+      msg: "Welcome to the Test",
+    },
+  });
+  
+  const buttonWrapper = wrapper.find({ ref: "switchTextButton" });
+  await buttonWrapper.trigger("click");
+
+  expect(wrapper.text()).toContain("Text Switched Successfully!!");
+});
